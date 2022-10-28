e2aae1918345c77a4e4d6fb50b5fcfb014e92c0c
jorgebaratech <93922396+jorgebaratech@users.noreply.github.com>
2022-10-28 08:27:40 +0200

    Add files via upload

---
+++ TextEditorGui.java
@@ -0,0 +1,332 @@
+package popehiflo;
+
+import java.awt.Color;
+import java.awt.FileDialog;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Document;
+import javax.swing.text.Highlighter;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ * @author popehiflo
+ */
+public class TextEditorGui extends javax.swing.JFrame {
+
+    String fileName;
+    Clipboard clipboard = getToolkit().getSystemClipboard();
+    
+    public TextEditorGui() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        searchButton = new javax.swing.JButton();
+        searchTextField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textArea = new javax.swing.JTextArea();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        newFileMenuItem = new javax.swing.JMenuItem();
+        openFileMenuItem = new javax.swing.JMenuItem();
+        saveFileMenuItem = new javax.swing.JMenuItem();
+        exitMenuItem = new javax.swing.JMenuItem();
+        editMenu = new javax.swing.JMenu();
+        cutTextMenuItem = new javax.swing.JMenuItem();
+        copyTextMenuItem = new javax.swing.JMenuItem();
+        pasteTextMenuItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        searchButton.setText("Search");
+        searchButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchButtonActionPerformed(evt);
+            }
+        });
+
+        textArea.setColumns(20);
+        textArea.setRows(5);
+        jScrollPane1.setViewportView(textArea);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(searchTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 586, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(searchButton)
+                .addContainerGap())
+            .addComponent(jScrollPane1)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(searchButton)
+                    .addComponent(searchTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 644, Short.MAX_VALUE))
+        );
+
+        fileMenu.setText("File");
+
+        newFileMenuItem.setText("New");
+        newFileMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                newFileMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(newFileMenuItem);
+
+        openFileMenuItem.setText("Open");
+        openFileMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                openFileMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(openFileMenuItem);
+
+        saveFileMenuItem.setText("Save");
+        saveFileMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveFileMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(saveFileMenuItem);
+
+        exitMenuItem.setText("Exit");
+        exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(exitMenuItem);
+
+        jMenuBar1.add(fileMenu);
+
+        editMenu.setText("Edit");
+
+        cutTextMenuItem.setText("Cut text");
+        cutTextMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cutTextMenuItemActionPerformed(evt);
+            }
+        });
+        editMenu.add(cutTextMenuItem);
+
+        copyTextMenuItem.setText("Copy text");
+        copyTextMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                copyTextMenuItemActionPerformed(evt);
+            }
+        });
+        editMenu.add(copyTextMenuItem);
+
+        pasteTextMenuItem.setText("Paste text");
+        pasteTextMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pasteTextMenuItemActionPerformed(evt);
+            }
+        });
+        editMenu.add(pasteTextMenuItem);
+
+        jMenuBar1.add(editMenu);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void cutTextMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cutTextMenuItemActionPerformed
+        String cutString = textArea.getSelectedText();
+        StringSelection cutSelection = new StringSelection(cutString);
+        clipboard.setContents(cutSelection, cutSelection);
+        textArea.replaceRange("", textArea.getSelectionStart(), textArea.getSelectionEnd());
+    }//GEN-LAST:event_cutTextMenuItemActionPerformed
+
+    private void newFileMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newFileMenuItemActionPerformed
+        textArea.setText("");
+        setTitle(fileName);
+    }//GEN-LAST:event_newFileMenuItemActionPerformed
+
+    private void openFileMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_openFileMenuItemActionPerformed
+        FileDialog fileDialog = new FileDialog(TextEditorGui.this, "Open File", FileDialog.LOAD);
+        fileDialog.setVisible(true);
+        
+        if(fileDialog.getFile() != null){
+            fileName = fileDialog.getDirectory() + fileDialog.getFile();
+            setTitle(fileName);
+        }
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            String line = null;
+            
+            while ((line = bufferedReader.readLine()) != null) {                
+                stringBuilder.append(line + "\n");
+                textArea.setText(stringBuilder.toString());
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println("File Not Found!");
+        }
+    }//GEN-LAST:event_openFileMenuItemActionPerformed
+
+    private void saveFileMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveFileMenuItemActionPerformed
+        FileDialog fileDialog = new FileDialog(TextEditorGui.this, "Save File", FileDialog.SAVE);
+        fileDialog.setVisible(true);
+        
+        if(fileDialog.getFile() != null){
+            fileName = fileDialog.getDirectory() + fileDialog.getFile();
+            setTitle(fileName);
+        }
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+            fileWriter.write(textArea.getText());
+            setTitle(fileName);
+            fileWriter.close();
+            
+        } catch (IOException e) {
+            System.out.println("File Not Found");
+        }
+    }//GEN-LAST:event_saveFileMenuItemActionPerformed
+
+    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitMenuItemActionPerformed
+
+    private void copyTextMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_copyTextMenuItemActionPerformed
+        String copyText = textArea.getSelectedText();
+        StringSelection copySelection = new StringSelection(copyText);
+        clipboard.setContents(copySelection, copySelection);
+    }//GEN-LAST:event_copyTextMenuItemActionPerformed
+
+    private void pasteTextMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pasteTextMenuItemActionPerformed
+        try {
+            Transferable pasteText = clipboard.getContents(TextEditorGui.this);
+            String sel = (String) pasteText.getTransferData(DataFlavor.stringFlavor);
+            textArea.replaceRange(sel, textArea.getSelectionStart(), textArea.getSelectionEnd());
+        } catch (Exception e) {
+            System.out.println("Did not work!");
+        }
+    }//GEN-LAST:event_pasteTextMenuItemActionPerformed
+
+    private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
+        searchTextArea(textArea, searchTextField.getText());
+    }//GEN-LAST:event_searchButtonActionPerformed
+    
+    class myHighlighter extends DefaultHighlighter.DefaultHighlightPainter {
+        
+        public myHighlighter(Color c) {
+            super(c);
+        }
+        
+    }
+    
+    DefaultHighlighter.HighlightPainter highlightPainter = new myHighlighter(Color.yellow);
+    public void removeHighLight(JTextComponent textComp) {
+        Highlighter removeHighlighter = textComp.getHighlighter();
+        Highlighter.Highlight[] remove = removeHighlighter.getHighlights();
+        
+        for(int i = 0; i < remove.length; i++) {
+            if(remove[i].getPainter() instanceof myHighlighter) {
+                removeHighlighter.removeHighlight(remove[i]);
+            }
+        }
+    }
+    
+    public void searchTextArea(JTextComponent textComp, String textString){
+        removeHighLight(textComp);
+        try {
+            Highlighter highlighter = textComp.getHighlighter();
+            Document doc = textComp.getDocument();
+            String text = doc.getText(0, doc.getLength());
+            
+            int pos = 0;
+            
+            while ((pos = text.toUpperCase().indexOf(textString.toUpperCase(), pos)) >= 0){
+                highlighter.addHighlight(pos, pos + textString.length(), highlightPainter);
+                pos += textString.length();
+            }
+        } catch (Exception e) {
+        }
+    }
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TextEditorGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TextEditorGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TextEditorGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TextEditorGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TextEditorGui().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem copyTextMenuItem;
+    private javax.swing.JMenuItem cutTextMenuItem;
+    private javax.swing.JMenu editMenu;
+    private javax.swing.JMenuItem exitMenuItem;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JMenuItem newFileMenuItem;
+    private javax.swing.JMenuItem openFileMenuItem;
+    private javax.swing.JMenuItem pasteTextMenuItem;
+    private javax.swing.JMenuItem saveFileMenuItem;
+    private javax.swing.JButton searchButton;
+    private javax.swing.JTextField searchTextField;
+    private javax.swing.JTextArea textArea;
+    // End of variables declaration//GEN-END:variables
+}
